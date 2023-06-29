b6f8c38aa3a1ac025804607fc688a5209615c6b4
Nataliia-SoffiaMonetsDev <nataliiasofiia.monets.dev@gmail.com>
2023-06-29 13:34:43 +0300

    changed front url to enableCors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:4200',
+        origin: 'http://products-list-task.s3-website.eu-central-1.amazonaws.com',
         credentials: true,
     });
     app.useGlobalPipes(createValidationPipe());
